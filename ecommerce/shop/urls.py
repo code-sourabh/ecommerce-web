3e57ea3942367d0dc736329afc2e69c42ea1785a
sourabh <sourabhmishra1262@gmail.com>
2021-05-12 23:23:49 +0530

    code version 22

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name='index'),
+    path('about/', views.about,name="AboutUs"),
+    path('contact/', views.contact,name="ContactUs"),
+    path('tracker', views.tracker,name="TrackingStatus"),
+    path('search/', views.search,name="Search"),
+    path('productview/', views.productView,name="TrackingStatus"),
+    path('checkout/', views.checkout,name="Checkout"),
 ]

dda63747f5d6b9effc16464777220faf311dbeb5
sourabh <sourabhmishra1262@gmail.com>
2021-05-13 16:41:21 +0530

    Basic Model for the Shop App has been created

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=200)
+    pub_date = models.DateField()

04a29a3d9b17bd5096fc4151a8325eb7b3f4c475
sourabh <sourabhmishra1262@gmail.com>
2021-05-14 22:59:08 +0530

    #33  product details showing from db, logic for slide, template reference done

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from shop.models import Product
+from math import ceil
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil(n/4 - n//4)
+    params = {'product': products , 'no_of_slides': nSlides , 'range': range(1 , nSlides)}
+    return render(request, 'shop/index.html' , params)
 
 def about(request):
     return render(request, 'shop/about.html')

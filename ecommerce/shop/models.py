3f066a4efcb77ff82e2f22695e4b16be6e40336d
sourabh <sourabhmishra1262@gmail.com>
2021-05-14 10:01:57 +0530

    model created,static media url dir made, web done

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -3,5 +3,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50 , default="")
+    subcategory = models.CharField(max_length=50 , default="")
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=200)
     pub_date = models.DateField()
+    image = models.ImageField(upload_to="shop/images/" , default="")
+
+    def __str__(self):
+        return self.product_name

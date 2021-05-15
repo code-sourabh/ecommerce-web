46102c8b507f1af06a067b5441aa23a05909e131
sourabh <sourabhmishra1262@gmail.com>
2021-05-16 00:49:08 +0530

    #35 showing the products category wise,  all comments on pages done , views, index.html changed

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,14 +1,40 @@
+import os
+import django
 from django.shortcuts import render
 from django.http import HttpResponse
 from shop.models import Product
 from math import ceil
 
+# Allowing sync_to_asyn so as to retrieve the data from the database
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup() 
+
+# function for the index.html file
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil(n/4 - n//4)
-    params = {'product': products , 'no_of_slides': nSlides , 'range': range(1 , nSlides)}
+
+# list of all the products      # cats: category list
+    prod = Product.objects.all()                
+    cats = []                                  
+
+# cats: list of all the UNIQUE categories of the product.
+    for i in range(len(prod)):
+        if prod[i].category not in cats:
+            cats.append(prod[i].category)
+
+# allProds: it is list of list of products category wise
+    allProds = []
+    for j in cats:
+        product = Product.objects.filter(category = j)
+
+# number of products of some category       # number of slides for product of some category
+        n = len(product)                                        
+        nSlides = n//4 + ceil(n/4 - n//4)                       
+        allProds.append([product , range(1, nSlides) , nSlides])
+
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html' , params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
